a40463a489d4039ae1e14b4541736efa389b79e4
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2020-02-29 17:55:49 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 # Initial Software
 sudo pacman -Syu
 
-sudo pacman -S --needed base-devel hwloc openssl cmake libmicrohttpd net-tools htop git nmap neofetch os-prober\
-networkmanager openssh-server sshfs openvpn openvpn-networkmanager openjdk-11-jre nano grub-customizer \
+sudo pacman -S --needed base-devel hwloc openssl cmake libmicrohttpd net-tools htop git nmap neofetch os-prober \
+networkmanager openssh-server sshfs openvpn networkmanager-openvpn jdk-openjdk nano grub-customizer \
 terminator transmission reflector flashplugin-installer lame audacity mpv vlc cmus gthumb soundconverter solaar \
 gparted gnome-tweaks gnome-tweak-tool dconf-editor xarchiver geany numactl msr-tools timeshift \
 virtualbox virtualbox-host-dkms virtualbox-guest-iso virtualbox-ext-pack android-tools \
-chrome-gnome-shell chromium-browser tor nextcloud-desktop -yy
+chrome-gnome-shell chromium-browser tor nextcloud-client -yy
 
 # Install Yay
 git clone https://aur.archlinux.org/yay.git
