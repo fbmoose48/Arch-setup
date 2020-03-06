0fc2f897e14c53481660fb6c6260775531baf61a
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2020-03-06 22:09:18 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
 # Initial Software
 sudo pacman -Syu
 
-sudo pacman -S --needed networkmanager openssh sshfs openvpn networkmanager-openvpn net-tools git nmap \ #connectivity
-nano htop grub-customizer terminator neofetch reflector os-prober solaar android-tools \ #tools
-base-devel hwloc openssl cmake libmicrohttpd numactl msr-tools \ #crypto/compile
-chromium-browser tor jdk-openjdk transmission flashplugin-installer lame vlc soundconverter \ #web/media
-gparted gnome-tweaks gnome-tweak-tool dconf-editor xarchiver geany chrome-gnome-shell \ #gnome tools
+sudo pacman -S --needed networkmanager openssh sshfs openvpn networkmanager-openvpn net-tools git nmap \ 
+nano htop grub-customizer terminator neofetch reflector os-prober solaar android-tools \ 
+base-devel hwloc openssl cmake libmicrohttpd numactl msr-tools \ 
+chromium-browser tor jdk-openjdk transmission flashplugin-installer lame vlc soundconverter \ 
+gparted gnome-tweaks gnome-tweak-tool dconf-editor xarchiver geany chrome-gnome-shell \ 
 virtualbox virtualbox-host-dkms nextcloud-client -yy
 
 # Other tools
-sudo pacman -S audacity mpv cmus gthumb timeshift virtualbox-guest-iso virtualbox-ext-pack tcpdump \ #media/virtualization
+sudo pacman -S audacity mpv cmus gthumb timeshift virtualbox-guest-iso virtualbox-ext-pack tcpdump \ 
 dosfstools mstools hdparm -yy #disk/fs tools
 
 # Install Yay
