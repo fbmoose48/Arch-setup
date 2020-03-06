35669d170ac5335f701c711a9ccee54248096c7f
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2020-03-06 16:59:04 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@ gparted gnome-tweaks gnome-tweak-tool dconf-editor xarchiver geany chrome-gnome-
 virtualbox virtualbox-host-dkms nextcloud-client -yy
 
 # Other tools
-sudo pacman -S audacity mpv vlc cmus gthumb timeshift virtualbox-guest-iso virtualbox-ext-pack tcpdump hdparm -yy
+sudo pacman -S audacity mpv vlc cmus gthumb timeshift virtualbox-guest-iso virtualbox-ext-pack tcpdump \ #media/virtualization
+dosfstools mstools hdparm -yy #disk/fs tools
 
 # Install Yay
 git clone https://aur.archlinux.org/yay.git
