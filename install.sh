886e6804cf582b3c9bcbd252752f6c5e28cdeb77
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2020-03-06 20:53:40 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ gparted gnome-tweaks gnome-tweak-tool dconf-editor xarchiver geany chrome-gnome-
 virtualbox virtualbox-host-dkms nextcloud-client -yy
 
 # Other tools
-sudo pacman -S audacity mpv vlc cmus gthumb timeshift virtualbox-guest-iso virtualbox-ext-pack tcpdump \ #media/virtualization
+sudo pacman -S audacity mpv cmus gthumb timeshift virtualbox-guest-iso virtualbox-ext-pack tcpdump \ #media/virtualization
 dosfstools mstools hdparm -yy #disk/fs tools
 
 # Install Yay
