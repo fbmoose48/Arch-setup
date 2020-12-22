a5caab6e2dd701d955842949be8ef1a731d593f3
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2020-12-22 16:17:09 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,41 +4,41 @@
 sudo pacman -Syu
 
 # Base system
-sudo pacman -S --needed gparted git patch nano htop powertop grub-customizer terminator neofetch reflector os-prober solaar xarchiver nnn s-tui progress -y
+sudo pacman -S --needed gparted git patch nano htop powertop grub-customizer terminator neofetch reflector os-prober solaar xarchiver nnn s-tui progress
 
 # Base WM
-sudo pacman -S --needed nitrogen gtk-chtheme picom lxterminal stalonetray xorg-xrandr xorg-xsetroot pasystray blueman -y
+sudo pacman -S --needed nitrogen gtk-chtheme picom lxterminal stalonetray xorg-xrandr xorg-xsetroot pasystray blueman
 
 # Network
-sudo pacman -S --needed networkmanager network-manager-applet dhclient openssh sshfs openvpn networkmanager-openvpn net-tools nmap firejail -y
+sudo pacman -S --needed networkmanager network-manager-applet dhclient openssh sshfs openvpn networkmanager-openvpn net-tools nmap firejail
 
 # Build tools
-sudo pacman -S --needed base-devel hwloc openssl cmake libmicrohttpd numactl msr-tools geany -y
+sudo pacman -S --needed base-devel hwloc openssl cmake libmicrohttpd numactl msr-tools geany
 
 # Coreboot
-sudo pacman -S --needed python gcc-ada flex bison ncurses wget zlib acpica -y
+sudo pacman -S --needed python gcc-ada flex bison ncurses wget zlib acpica
 
 # Gnome tools
-#sudo pacman -S --needed gnome-tweaks gnome-tweak-tool dconf-editor chrome-gnome-shell  -y
+#sudo pacman -S --needed gnome-tweaks gnome-tweak-tool dconf-editor chrome-gnome-shell
 
 # Web
-sudo pacman -S --needed tor jdk-openjdk transmission-gtk flashplugin chromium nextcloud-client -y
+sudo pacman -S --needed tor jdk-openjdk transmission-gtk flashplugin chromium nextcloud-client
 
 # Media 
-sudo pacman -S --needed lame gthumb vlc cmus audacity soundconverter pulseeffects ncmpcpp -y
+sudo pacman -S --needed lame gthumb vlc cmus audacity soundconverter pulseeffects ncmpcpp
 
 # Fonts
-sudo pacman -S --needed ttf-bitstream-vera ttf-dejavu powerline-fonts -y
+sudo pacman -S --needed ttf-bitstream-vera ttf-dejavu powerline-fonts
 
 # Laptop
-#sudo pacman -S tlp -y
+#sudo pacman -S --needed tlp
 
 # Virtualization
-#sudo pacman -S --needed virtualbox virtualbox-host-modules-arch virtualbox-guest-iso virtualbox-guest-utils -y
+#sudo pacman -S --needed virtualbox virtualbox-host-modules-arch virtualbox-guest-iso virtualbox-guest-utils
 #sudo modprobe vboxdrv
 
 # Other tools
-#sudo pacman -S android-tools mpv cmus timeshift dosfstools mstools hdparm tcpdump -y
+#sudo pacman -S android-tools mpv cmus timeshift dosfstools mstools hdparm tcpdump
 
 # Fix DHCP on NetworkManager
 #sudo echo "[main]" >> /etc/NetworkManager/conf.d/dhcp-client.conf
@@ -59,10 +59,10 @@ sudo pacman -S --needed ttf-bitstream-vera ttf-dejavu powerline-fonts -y
 yay -Syu
 
 # AUR Software
-yay -S --needed downgrade spacefm inxi tor-browser teamviewer wireshark-git cli-visualizer imagewriter -y
+yay -S --needed downgrade spacefm inxi tor-browser teamviewer wireshark-git cli-visualizer imagewriter
 
 # AUR Fonts
-yay -S --needed nerd-fonts-source-code-pro nerd-fonts-liberation-mono lexend-fonts-git libxft-bgra-git ttf-fixedsys-excelsior-linux nerd-fonts-profont-windows -y
+yay -S --needed nerd-fonts-source-code-pro nerd-fonts-liberation-mono lexend-fonts-git libxft-bgra-git ttf-fixedsys-excelsior-linux nerd-fonts-profont-windows
 
 # AUR laptop
 #yay -S --needed thinkfan
