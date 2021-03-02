c370fb5d9de736cabf0acbbf4fbf75b16467f9d9
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2021-03-02 05:25:58 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,16 +16,16 @@ sudo pacman -S --needed networkmanager network-manager-applet dhclient openssh s
 sudo pacman -S --needed base-devel hwloc openssl cmake libmicrohttpd numactl msr-tools geany
 
 # Coreboot
-sudo pacman -S --needed python gcc-ada flex bison ncurses wget zlib acpica
+#sudo pacman -S --needed python gcc-ada flex bison ncurses wget zlib acpica
 
 # Gnome tools
 #sudo pacman -S --needed gnome-tweaks gnome-tweak-tool dconf-editor chrome-gnome-shell
 
 # Web
-sudo pacman -S --needed tor jdk-openjdk transmission-gtk flashplugin chromium nextcloud-client
+#sudo pacman -S --needed firefox thunderbird tor jdk-openjdk transmission-gtk flashplugin chromium nextcloud-client
 
 # Media 
-sudo pacman -S --needed lame gthumb vlc cmus audacity soundconverter pulseeffects ncmpcpp
+#sudo pacman -S --needed lame gthumb vlc cmus audacity soundconverter pulseeffects ncmpcpp
 
 # Fonts
 sudo pacman -S --needed ttf-bitstream-vera ttf-dejavu powerline-fonts
@@ -56,10 +56,10 @@ sudo pacman -S --needed ttf-bitstream-vera ttf-dejavu powerline-fonts
 #makepkg -si
 
 # AUR Update
-yay -Syu
+#yay -Syu
 
 # AUR Software
-yay -S --needed downgrade spacefm inxi tor-browser teamviewer wireshark-git cli-visualizer imagewriter
+#yay -S --needed downgrade spacefm inxi tor-browser teamviewer wireshark-git cli-visualizer imagewriter
 
 # AUR Fonts
 yay -S --needed nerd-fonts-source-code-pro nerd-fonts-liberation-mono lexend-fonts-git libxft-bgra-git ttf-fixedsys-excelsior-linux nerd-fonts-profont-windows
@@ -84,24 +84,24 @@ sudo systemctl start fstrim.timer
 wget -O - https://github.com/shvchk/poly-dark/raw/master/install.sh | bash 
 
 #Make it SUCKLESS
-cd ~
-git clone https://github.com/fbmooose48/dwm.git
-cd dwm
-sudo make install
-sudo mv usr/share/xsessions/dwm.desktop /usr/share/xsessions
-sudo mv usr/local/bin/dwm-start.sh /usr/local/bin
-sudo chmod +x /usr/local/bin/dwm-start.sh
-sudo pacman -S --needed libxft ttf-hack ttf-joypixels picom xorg-xrandr xorg-xsetroot nitrogen stalonetray network-manager-applet pasystray blueman
-yay -S --needed nerd-fonts-source-code-pro libxft-bgra-git
-cd ~
-git clone https://github.com/fbmooose48/st.git
-cd st
-sudo make install
-cd ~
-git clone https://github.com/fbmooose48/dmenu.git
-cd dmenu
-sudo make install
-cd ~
-git clone https://github.com/fbmooose48/slock.git
-cd slock
-sudo make install
+#cd ~
+#git clone https://github.com/fbmooose48/dwm.git
+#cd dwm
+#sudo make install
+#sudo mv usr/share/xsessions/dwm.desktop /usr/share/xsessions
+#sudo mv usr/local/bin/dwm-start.sh /usr/local/bin
+#sudo chmod +x /usr/local/bin/dwm-start.sh
+#sudo pacman -S --needed libxft ttf-hack ttf-joypixels picom xorg-xrandr xorg-xsetroot nitrogen stalonetray network-manager-applet pasystray blueman
+#yay -S --needed nerd-fonts-source-code-pro libxft-bgra-git
+#cd ~
+#git clone https://github.com/fbmooose48/st.git
+#cd st
+#sudo make install
+#cd ~
+#git clone https://github.com/fbmooose48/dmenu.git
+#cd dmenu
+#sudo make install
+#cd ~
+#git clone https://github.com/fbmooose48/slock.git
+#cd slock
+#sudo make install
