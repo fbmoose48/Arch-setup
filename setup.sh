a4656bc41d67dd4adc3ee8e7218705a6cc9cee97
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2021-04-13 02:32:47 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,10 +7,10 @@ sudo pacman -Syu
 sudo pacman -S --needed git patch nano htop powertop sysstat grub-customizer neofetch gnome-keyring reflector os-prober nnn s-tui progress
 
 # Base WM
-sudo pacman -S --needed nitrogen gtk-chtheme picom lxterminal lxappearance stalonetray xorg-xrandr xorg-xsetroot libxft imlib2 pasystray blueman
+sudo pacman -S --needed nitrogen gtk-chtheme picom lxterminal lxappearance stalonetray xorg-xrandr xorg-xsetroot libxft imlib2 gparted terminator xarchiver
 
-# Base GUI tools
-sudo pacman -S --needed gparted terminator solaar xarchiver epdfview 
+# Expanded GUI tools
+#sudo pacman -S --needed pasystray blueman solaar epdfview 
 
 # Network
 sudo pacman -S --needed networkmanager network-manager-applet dhclient openssh sshfs openvpn networkmanager-openvpn net-tools nmap 
