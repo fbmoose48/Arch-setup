5676e1e07bc6155a6e19aa59280e64f96c85b330
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2021-05-05 05:06:38 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -65,8 +65,11 @@ sudo echo "dhcp=dhclient" >> /etc/NetworkManager/conf.d/dhcp-client.conf
 # AUR Update
 #yay -Syu
 
+# AUR utilities
+yay -S --needed spacefm imagewriter
+
 # AUR Software
-#yay -S --needed downgrade spacefm inxi tor-browser teamviewer wireshark-git cli-visualizer imagewriter
+#yay -S --needed downgrade inxi tor-browser teamviewer wireshark-git cli-visualizer 
 
 #AUR AMDGPU-PRO OpenCL drivers
 #yay -S --needed opencl-amd-polaris
@@ -94,24 +97,24 @@ sudo systemctl start fstrim.timer
 wget -O - https://github.com/shvchk/poly-dark/raw/master/install.sh | bash 
 
 #Make it SUCKLESS
-#cd ~
-#git clone https://github.com/fbmooose48/dwm.git
-#cd dwm
-#sudo make install
-#sudo mv usr/share/xsessions/dwm.desktop /usr/share/xsessions
-#sudo mv usr/local/bin/dwm-start.sh /usr/local/bin
-#sudo chmod +x /usr/local/bin/dwm-start.sh
-#sudo pacman -S --needed libxft ttf-hack ttf-joypixels picom xorg-xrandr xorg-xsetroot nitrogen stalonetray network-manager-applet pasystray blueman
-#yay -S --needed nerd-fonts-source-code-pro libxft-bgra-git
-#cd ~
-#git clone https://github.com/fbmooose48/st.git
-#cd st
-#sudo make install
-#cd ~
-#git clone https://github.com/fbmooose48/dmenu.git
-#cd dmenu
-#sudo make install
-#cd ~
-#git clone https://github.com/fbmooose48/slock.git
-#cd slock
-#sudo make install
+cd ~
+git clone https://github.com/fbmooose48/dwm.git
+cd dwm
+sudo make install
+sudo mv usr/share/xsessions/dwm.desktop /usr/share/xsessions
+sudo mv usr/local/bin/dwm-start.sh /usr/local/bin
+sudo chmod +x /usr/local/bin/dwm-start.sh
+sudo pacman -S --needed libxft ttf-hack ttf-joypixels picom xorg-xrandr xorg-xsetroot nitrogen stalonetray network-manager-applet pasystray blueman
+yay -S --needed nerd-fonts-source-code-pro libxft-bgra-git
+cd ~
+git clone https://github.com/fbmooose48/st.git
+cd st
+sudo make install
+cd ~
+git clone https://github.com/fbmooose48/dmenu.git
+cd dmenu
+sudo make install
+cd ~
+git clone https://github.com/fbmooose48/slock.git
+cd slock
+sudo make install
