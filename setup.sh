e0c012b759bbba264ea956db77a99539852b8609
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2021-05-14 04:20:43 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -95,26 +95,3 @@ sudo systemctl start fstrim.timer
 
 # Poly Dark GRUB theme
 wget -O - https://github.com/shvchk/poly-dark/raw/master/install.sh | bash 
-
-#Make it SUCKLESS
-#cd ~
-#git clone https://github.com/fbmooose48/dwm.git
-#cd dwm
-#sudo make install
-#sudo mv usr/share/xsessions/dwm.desktop /usr/share/xsessions
-#sudo mv usr/local/bin/dwm-start.sh /usr/local/bin
-#sudo chmod +x /usr/local/bin/dwm-start.sh
-#sudo pacman -S --needed libxft ttf-hack ttf-joypixels picom xorg-xrandr xorg-xsetroot nitrogen stalonetray network-manager-applet pasystray blueman
-#yay -S --needed nerd-fonts-source-code-pro libxft-bgra-git
-#cd ~
-#git clone https://github.com/fbmooose48/st.git
-#cd st
-#sudo make install
-#cd ~
-#git clone https://github.com/fbmooose48/dmenu.git
-#cd dmenu
-#sudo make install
-#cd ~
-#git clone https://github.com/fbmooose48/slock.git
-#cd slock
-#sudo make install
