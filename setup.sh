dc5b54a5d31329334d755c8bfc15788bb8a2afbd
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2021-09-29 16:23:15 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 sudo pacman -Syu
 
 # Base system
-sudo pacman -S --needed git patch nano htop powertop sysstat neofetch gnome-keyring reflector os-prober nnn s-tui progress udisks2
+sudo pacman -S --needed git patch nano htop powertop sysstat neofetch gnome-keyring reflector os-prober nnn s-tui progress udisks2 detox
 
 # Base WM
 sudo pacman -S --needed nitrogen gtk-chtheme picom lxterminal lxappearance stalonetray xorg-xrandr xorg-xsetroot libxft imlib2 gparted terminator xarchiver grub-customizer
