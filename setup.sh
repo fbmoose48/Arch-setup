1733303aa76c1a2e500bb72e5456a8a71f53664c
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2021-11-09 17:52:14 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,40 +4,38 @@
 sudo pacman -Syu
 
 # Base system
-sudo pacman -S --needed git patch nano htop powertop sysstat neofetch nnn progress udisks2
-
-# Base WM
-#sudo pacman -S --needed nitrogen gtk-chtheme picom lxterminal lxappearance gparted xarchiver 
-
-# Expanded GUI tools
-#sudo pacman -S --needed pasystray blueman solaar epdfview geany
-
-# Network
-sudo pacman -S --needed openssh sshfs openvpn networkmanager-openvpn net-tools nmap speedtest-cli nfs-utils tcpdump
+sudo pacman -S --needed git patch nano htop powertop sysstat neofetch progress
 
 # Build tools
-sudo pacman -S --needed base-devel hwloc openssl cmake libmicrohttpd 
+sudo pacman -S --needed go base-devel hwloc openssl cmake
+
+# Phosh GUI
+sudo pacman -S --needed gparted xarchiver siglo gnome-calendar-mobile modem-manager-gui pavucontrol
+
+# Expanded GUI tools
+#sudo pacman -S --needed epdfview geany
+
+# Network
+#sudo pacman -S --needed net-tools nmap speedtest-cli nfs-utils tcpdump
 
 # File Sytem tools
-#sudo pacman -S --needed squashfs-tools f2fs-tools dosfstools mstools hdparm
+#sudo pacman -S --needed squashfs-tools f2fs-tools dosfstools mstools hdparm udisk2 nnn
 
 # Web
-#sudo pacman -S --needed firefox tor jdk-openjdk transmission-gtk 
-
-# Web expanded
-#sudo pacman -S --needed thunderbird flashplugin chromium nextcloud-client firejail
+#sudo pacman -S --needed firefox tor transmission-gtk 
 
 # Media 
-#sudo pacman -S --needed lame mpv pavucontrol
+#sudo pacman -S --needed lame mpv
 
 # Media expanded
 #sudo pacman -S --needed gthumb vlc cmus audacity soundconverter pulseeffects ncmpcpp
 
 # Other tools
-#sudo pacman -S android-tools mpv cmus timeshift 
+#sudo pacman -S android-tools timeshift 
 
 # Install Yay
 #git clone https://aur.archlinux.org/yay.git
+#sudo chown -R alarm:users ./yay
 #cd yay
 #makepkg -si
 
@@ -45,13 +43,19 @@ sudo pacman -S --needed base-devel hwloc openssl cmake libmicrohttpd
 #yay -Syu
 
 # AUR utilities
-yay -S --needed spacefm
+yay -S --needed mmsd-tng-git axolotl auther-git cotp
 
 # AUR Software
-#yay -S --needed downgrade inxi tor-browser teamviewer wireshark-git cli-visualizer 
+#yay -S --needed downgrade inxi tor-browser wireshark-git cli-visualizer spacefm
+
+# Cleanup
+sudo pacman -Rns gnome-software-mobile
 
 # Add $USER to necessary groups
 #sudo usermod -a -G video,lp,lpadmin $USER
+
+# Enableme experimental MMS in Chatty
+gsettings set sm.puri.Chatty experimental-features true
 
 #Enable fstrim
 #sudo systemctl enable fstrim.timer
